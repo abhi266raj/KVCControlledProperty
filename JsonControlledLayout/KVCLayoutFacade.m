2c0460d858e93bdaf5717397b346af5439ca02c0
Abhiraj <abhiraj@roposo.com>
2018-05-12 16:07:22 +0530

    First version

---
+++ JsonControlledLayout/KVCLayoutFacade.m
@@ -0,0 +1,45 @@
+//
+//  KVCLayoutFacade.m
+//  JsonControlledLayout
+//
+//  Created by AbhirajKumar on 5/12/18.
+//  Copyright © 2018 Personal Project. All rights reserved.
+//
+
+#import "KVCLayoutFacade.h"
+#import "KVCMapper.h"
+#import "DataFetcher.h"
+
+
+@interface KVCLayoutFacade()
+@property (nonatomic,strong) KVCMapper *mapper;
+@property (nonatomic,strong) DataFetcher *fetcher;
+@property (nonatomic,strong) NSDictionary *dataDictionary;
+@end
+
+@implementation KVCLayoutFacade
+
+
+-(id)init{
+    self = [super init];
+    if (self){
+        self.mapper = [[KVCMapper alloc]init];
+        self.fetcher = [[DataFetcher alloc]init];
+    }
+    return self;
+}
+
+-(void)fetchRequiredDataFromUrl:(NSString*)url WithCompletionHandler:(void (^)(void))completionHandler{
+    [self.fetcher fetchResourceFromUrl:url withCompletionHandler:^(NSError *error, NSDictionary *data) {
+        self.dataDictionary = data;
+    }];
+    
+}
+-(void)updateKVCDataOnObject:(NSObject*)object{
+    if (_dataDictionary){
+    [self.mapper updateKVCValueForObject:object usingDict:_dataDictionary];
+    }
+    
+}
+
+@end
